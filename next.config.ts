6429d82ae195e396c78c7f9fa5542b5764afff33
Amanda Hernandez <63179699+jumpmanda@users.noreply.github.com>
2025-05-04 16:27:32 -0700

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'dist',
+  output: "export",
+  distDir: "dist",
   images: {
     unoptimized: true,
-  }
+  },
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);
